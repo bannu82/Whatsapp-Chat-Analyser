2302213d966532345b8cef6445e50fd90e2353bb
Dev Shakeywar <122546955+bannu82@users.noreply.github.com>
2024-07-23 02:39:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,21 @@
 import streamlit as st
 import Preprocessor, helper
 import matplotlib.pyplot as plt
+import os
 import pandas as pd
+import re 
 
-st.set_page_config(layout='wide', initial_sidebar_state='expanded')
+st.set_page_config(layout='wide',initial_sidebar_state='expanded')
 st.sidebar.title('WhatsApp Chat Analyzer')
 
-# State management for collapsing sidebar
-if 'show_analysis' not in st.session_state:
-    st.session_state.show_analysis = False
 
 uploaded_file = st.sidebar.file_uploader('Choose a file', type=['txt'])
 
+
+
 # Title and Introduction
+
+
 if uploaded_file is None:
     st.markdown(helper.desc())
 
@@ -22,6 +25,10 @@ if uploaded_file is not None:
     
     df = Preprocessor.preprocess(data)
 
+    # # Save Files 
+    # val = helper.save_data_file(uploaded_file.name , df)
+    # st.title(val)
+    
     st.dataframe(df)
 
     # Verify the DataFrame columns
@@ -35,16 +42,13 @@ if uploaded_file is not None:
         user_list.insert(0, 'Overall')
 
         selected_user = st.sidebar.selectbox('Show analysis wrt', user_list)
-
-        if not st.session_state.show_analysis:
-            show_analysis = st.sidebar.button('Show Analysis')
-            hint = st.sidebar.success('Click "Show Analysis" Button')
-            
-            if show_analysis:
-                st.session_state.show_analysis = True
-                hint.empty()
-
-        if st.session_state.show_analysis:
+        
+        
+        show_analysis = st.sidebar.button('Show Analysis')
+        hint = st.sidebar.success('Click "Show Analyis" Button')
+        
+        if show_analysis:
+            hint.empty()
             cols1, cols2, cols3, cols4 = st.columns(4)
 
             no_msg, word, num_media, links = helper.find_stats(selected_user, df)
@@ -80,9 +84,10 @@ if uploaded_file is not None:
                 with col2:
                     st.dataframe(df_busy)
 
-            # WORD CLOUD
+
+#               WORD CLOUD
             try:
-                st.title('Word Cloud')
+                st.title('World Cloud')
                 df_wc = helper.create_cloud(selected_user, df)
                 fig, ax = plt.subplots()
                 ax.imshow(df_wc)
@@ -90,41 +95,61 @@ if uploaded_file is not None:
             except KeyError as e:
                 st.error(e)
 
-            # MOST COMMON WORDS
+#               MOST COMMON WORD 
             try:
                 st.title('Most Common Words')
-                most_common_df = helper.most_common_words(selected_user, df)
-                fig, ax = plt.subplots()
-                ax.barh(most_common_df[0], most_common_df[1])
+                most_common_df = helper.most_common_words(selected_user , df)
+                fig , ax = plt.subplots()
+                ax.barh(most_common_df[0]  , most_common_df[1])
                 st.pyplot(fig)
+                # st.dataframe(most_common_df)
+            
             except KeyError as e:
-                st.error(e)
+                st.error(e)                
 
-            # EMOJI COUNTER
+
+            # Emoji Counter
             try:
                 st.title('Emoji Counter')
-                emoji_df = helper.emoji_counter(selected_user, df)
+                emoji_df = helper.emoji_counter(selected_user , df)
 
-                col1, col2 = st.columns(2)
+                col1 , col2 = st.columns(2)
                 with col1:
-                    fig, ax = plt.subplots()
-                    ax.pie(emoji_df[1].head(5), labels=emoji_df[0].head(5), autopct='%0.2f')
+                    fig , ax = plt.subplots()
+                    ax.pie( emoji_df[1].head(5) , labels=emoji_df[0].head(5), autopct='%0.2f')
                     st.pyplot(fig)
 
                 with col2:
                     st.dataframe(emoji_df)
+
             except KeyError as e:
                 st.error(e)
 
         st.sidebar.divider()
-
+        
         # Word To Find
         w = st.sidebar.text_input("Word to find message ")
 
         if st.sidebar.button('word to find'):
+             
             try:
-                st.title('Word in Messages')
-                msg_df = helper.get_message(selected_user, df, w)
+                st.title('word in msgs')
+                
+                msg_df = helper.get_message(selected_user , df , w)
                 st.dataframe(msg_df)
+
+
             except KeyError as e:
                 st.error(e)
+        
+
+        # # Find By Column
+        # clmn = df.columns.tolist()
+
+        # selected_clmn = st.sidebar.selectbox('select column',clmn)
+        # if selected_clmn:
+        #     selected_data = st.sidebar.text_input('Enter ')
+
+        # if st.sidebar.button('find'):
+        #     st.dataframe(helper.get_data_by_clmn(selected_user , df , selected_clmn , selected_data))
+        
